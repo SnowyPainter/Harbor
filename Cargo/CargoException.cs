80f6a99892b3073891a7e14142002c2ba8bb12a4
SnowyPainter <SnowyPainter@gmail.com>
2020-10-25 12:13:33 +0900

    Add custom cargo exception.cs

--- Cargo/CargoException.cs
+++ Cargo/CargoException.cs
@@ -4,6 +4,11 @@ using System.Text;
 
 namespace ADPC.Cargo
 {
+    public static class CargoExceptionMsg
+    {
+        public static readonly string Locked = "Cargo has been already locked";
+        public static readonly string Empty = "Cargo is empty, so It can't be loaded to ship";
+    }
     public class CargoException:Exception
     {
         public CargoException()
