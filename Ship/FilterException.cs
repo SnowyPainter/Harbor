9bed5189d41f900cc9fdf2afe40204c59d31a71d
SnowyPainter <SnowyPainter@gmail.com>
2020-11-01 21:14:27 +0900

    Add filter for CargoReport

---
+++ Ship/FilterException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADPC.Ship
+{
+    public class FilterException: Exception
+    {
+        public FilterException()
+        {
+
+        }
+        public FilterException(string message)
+            : base(message)
+        {
+        }
+    }
+}
