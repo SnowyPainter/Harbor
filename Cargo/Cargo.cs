8652708dd0c4e15b74aec771b7b335d23cd49a81
SnowyPainter <SnowyPainter@gmail.com>
2020-12-27 15:58:08 +0900

    Combine classes that related to cargo&functions to abstract class Cargo.cs

---
+++ Cargo/Cargo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Harbor.Cargo
+{
+    [Serializable]
+    public abstract class Cargo
+    {
+        [XmlIgnore]
+        public bool IsLocked { get; private set; }
+
+        [XmlAttribute]
+        public readonly CargoType Type;
+        /// <summary>
+        /// Very Worth Data
+        /// PrimaryTime is the first time to load to ship. it preserve as filename.
+        /// </summary>
+        public DateTime? PrimaryTime { get; set; }
+
+        public Cargo(CargoType t)
+        {
+            Type = t;
+        }
+        public void SetPrimaryTimeNow()
+        {
+            PrimaryTime = DateTime.Now;
+        }
+        public abstract bool IsEmpty();
+        public void Lock()
+        {
+            IsLocked = true;
+        }
+        public void UnLock()
+        {
+            IsLocked = false;
+        }
+
+        public abstract byte[] ToPacket();
+    }
+}
