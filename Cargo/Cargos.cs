eb15c72a4dd8370677bdfe015bfa999d1fa1eb9e
SnowyPainter <SnowyPainter@gmail.com>
2021-01-25 10:35:14 +0900

    Seperate Log -> WPFLog, DataLog

--- Cargo/Cargos.cs
+++ Cargo/Cargos.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net.Mime;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
@@ -35,13 +36,21 @@ namespace Harbor.Cargo
     }
     #endregion
 
+    public interface IContentTypeContainer
+    {
+        Type ContentType { get; }
+    }
+
     #region Cargo ProtoContracts & Serializables
     [Serializable]
-    public class RawCargo : Cargo
+    public class RawCargo<T> : Cargo, IContentTypeContainer
     {
-        public byte[] Data { get; set; }
-
-        public RawCargo() : base(CargoType.GenericObject) { }
+        public List<T> Data { get; private set; }
+        public RawCargo() : base(CargoType.GenericObject) { Data = new List<T>(); }
+        public Type ContentType { get; private set; } = typeof(T);
+        public void Load(T data) {
+            Data.Add(data);
+        }
         public string GetRaw()
         {
             return Data.ToString();
@@ -54,7 +63,7 @@ namespace Harbor.Cargo
 
         public override byte[] ToPacket()
         {
-            var serializer = new XmlSerializer(typeof(RawCargo));
+            var serializer = new XmlSerializer(typeof(RawCargo<T>));
             string xml = "";
             using (var writer = new StringWriter())
             {
@@ -123,30 +132,28 @@ namespace Harbor.Cargo
         }
     }
     [Serializable]
-    public class LogCargo : Cargo
+    public class DataLogCargo : Cargo
     {
-        public Stack<IActivityLog> Logs { get; private set; }
-
-        public LogCargo():base(CargoType.Log)
+        public Stack<DataLog> Logs { get; private set; }
+        public DataLogCargo():base(CargoType.Log)
         {
-            Logs = new Stack<IActivityLog>();
+            Logs = new Stack<DataLog>();
         }
-
-        public void Load(DateTime time, FrameworkElement element, Point mousePoint)
+        public void Load(DateTime time, string c)
         {
             if (!IsLocked)
-                Logs.Push(new ActivityLog(time, element, mousePoint));
+                Logs.Push(new DataLog(time, c));
             else
                 throw new CargoException(CargoExceptionMsg.Locked);
         }
-        public void Load(IActivityLog log)
+        public void Load(DataLog log)
         {
             if (!IsLocked)
                 Logs.Push(log);
             else
                 throw new CargoException(CargoExceptionMsg.Locked);
         }
-        public Stack<IActivityLog> GetLogs()
+        public Stack<DataLog> GetLogs()
         {
             return Logs;
         }
@@ -157,7 +164,52 @@ namespace Harbor.Cargo
         }
         public override byte[] ToPacket()
         {
-            var serializer = new XmlSerializer(typeof(LogCargo));
+            var serializer = new XmlSerializer(typeof(WPFLogCargo));
+            string xml = "";
+            using (var writer = new StringWriter())
+            {
+                serializer.Serialize(writer, this);
+                xml = writer.ToString();
+            }
+            return Encoding.UTF8.GetBytes(xml);
+        }
+    }
+    [Serializable]
+    public class WPFLogCargo : Cargo
+    {
+        public Stack<WPFActivityLog> Logs { get; private set; }
+
+        public WPFLogCargo():base(CargoType.Log)
+        {
+            Logs = new Stack<WPFActivityLog>();
+        }
+
+        public void Load(DateTime time, FrameworkElement element, Point mousePoint)
+        {
+            if (!IsLocked)
+                Logs.Push(new WPFActivityLog(time, element, mousePoint));
+            else
+                throw new CargoException(CargoExceptionMsg.Locked);
+        }
+        public void Load(WPFActivityLog log)
+        {
+            if (!IsLocked)
+                Logs.Push(log);
+            else
+                throw new CargoException(CargoExceptionMsg.Locked);
+        }
+        public Stack<WPFActivityLog> GetLogs()
+        {
+            return Logs;
+        }
+
+        public override bool IsEmpty()
+        {
+            return Logs.Count <= 0 ? true : false;
+        }
+        public override byte[] ToPacket()
+        {
+            var serializer = new XmlSerializer(typeof(WPFLogCargo));
             string xml = "";
             using (var writer = new StringWriter())
             {
